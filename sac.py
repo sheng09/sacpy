b41ed1bfb204bd5e4a3074c41d241c634527591c
sheng09 <wangsheng.cas@gmail.com>
2019-05-27 15:25:04 +1000

    Update sac.py with new methods and debugs

--- sac.py
+++ sac.py
@@ -18,6 +18,10 @@ File IO dependent methods
 >>> s.taper(0.02) # ratio can be 0 ~ 0.5
 >>> s.bandpass(0.5, 2.0, order= 4, npass= 2)
 >>> s.write('1_new.sac')
+>>> 
+>>> # arbitrary plot
+>>> ts = s.get_time_axis()
+>>> plt.plot(ts, s['dat'], color='black') # ...
 >>>
 >>> # cut and read
 >>> s = rd_sac_2('1.sac', 'e', -50, -20)
@@ -32,7 +36,7 @@ Arbitrary data writing, and processing methods
 >>> dat = np.random.random(1000)
 >>> delta, b = 0.05, 50.0
 >>> # writing method 1
->>> write_sac_2('junk.sac', dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
+>>> wrt_sac_2('junk.sac', dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
 >>> 
 >>> # writing method 2, with processings
 >>> s = make_sactrace_v(dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
@@ -50,7 +54,7 @@ Sac header update and revision
 >>> new_hdr['t1'] = 2.0 # meaningless value, just for example
 >>> new_hdr['t2'] = 4.0
 >>> new_hdr.update( **{'stlo': -10.0, 'stla': 10.0, 'delta': 0.2 } )
->>> s_new = make_sactrace_hdr(s.dat, new_hdr)
+>>> s_new = make_sactrace_hdr(s['dat'], new_hdr)
 >>> s_new.write('1_new.sac')
 
 
@@ -59,9 +63,14 @@ Massive data IO and processing
 
 >>> # read, process, and write a bunch of sac files in 3 lines
 >>> fnm_lst = ['1.sac', '2.sac', '3.sac']
->>> vol = [rd_sac(fnm).detrend().taper().lowpass(1.2, order= 2, npass= 2) for fnm in fnm_lst]
->>> [ s.write(s['filename'].replace('.sac', '_proced.sac') ) for s in vol ]
-
+>>> for fnm in fnm_lst:
+...     s = rd_sac(fnm)
+...     s.detrend()
+...     s.taper()
+...     s.lowpass(1.2, order= 2, npass= 2)
+...     s.write(s['filename'].replace('.sac', '_proced.sac') )
+...
+>>> 
 
 
 """
@@ -86,7 +95,8 @@ def rd_sac(filename):
 def rd_sac_2(filename, tmark, t1, t2):
     """
     Read sac data given filename and time window, and return an object ot sactrace.
-    tmakr: 'b', 'e', 'o', 'a', 't0', 't1', ... 't9'.
+    tmakr: 'b', 'e', 'o', 'a', 't0', 't1', ... 't9';
+    t1, t2: float
     """
     tmp = sactrace()
     tmp.read_2(filename, tmark, t1, t2)
@@ -126,14 +136,14 @@ def make_sactrace_v(dat, delta, b, **kwargs):
     tmp = sactrace()
     tmp.init(dat, hdr)
     return tmp
-def write_sac(filename, dat, hdr):
+def wrt_sac(filename, dat, hdr):
     """
     Write sac file given dat (an object of numpy.ndarray) and hdr (an object of sachdr)
     """
     tmp = sactrace()
     tmp.init(dat, hdr, False)
     tmp.write(filename)
-def write_sac_2(filename, dat, delta, b, **kwargs):
+def wrt_sac_2(filename, dat, delta, b, **kwargs):
     """
     Write sac file given dat (an object of numpy.ndarray) and header settings.
     """
@@ -226,6 +236,7 @@ class sachdr:
               fid (default)
         """
         if type == 'filename':
+            self.d_arch['filename'] = copy.deepcopy(f)
             f = open(f, 'rb')
         hdrvol = f.read(632)
         info = struct.unpack('70f40i192s', hdrvol)
@@ -273,16 +284,31 @@ class sachdr:
         """
         lst = [self.d_arch[k] for k in self.f_keys ]
         lst.extend( [self.d_arch[k] for k in self.i_keys ] )
-        lst.append( ''.join([self.d_arch[k] for k in self.s_keys ]).encode('unicode_escape') )
+        s = '{:\0<8.8}{:\0<16.16}'.format(self.d_arch['kstnm'], self.d_arch['kevnm'] )
+        s += ''.join( ['{:\0<8.8}'.format(self.d_arch[k]) for k in self.s_keys[2:] ] )
+        lst.append( bytes(s, 'utf8') )
         return struct.pack('70f40i192s', *lst)
     ###
     def update(self, **kwargs):
         """
         Update sac hdr information.
+        kwargs: dict of (key: value), eg: {'delta': 0.1, 'kstnm': 'HYD}
         """
         for k, v in kwargs.items():
             self.d_arch[k] = v
     ###
+    def __update_npts__(self, npts):
+        """
+        update npts, and associated e.
+        """
+        self.d_arch['npts'] = npts
+        self.d_arch['e'] = (npts-1) * self.d_arch['delta'] * self.d_arch['delta']
+    def __update_b__(self, b):
+        """
+        Update b, and dependent e.
+        """
+        self.d_arch['e'] = b - self.d_arch['b'] + self.d_arch['e']
+        self.d_arch['b'] = b
     def __getitem__(self, key):
         """
         Accessing with specified key.
@@ -302,10 +328,10 @@ class sachdr:
         keys = set(self.d_arch.keys())
         sub_keys = keys - set(sachdr.all_keys)
         s_lst = []
-        s_lst.extend( ['internal %-9s: %f' % (k, self.d_arch[k]) for k in sachdr.f_keys] )
-        s_lst.extend( ['internal %-9s: %d' % (k, self.d_arch[k]) for k in sachdr.i_keys] )
-        s_lst.extend( ['internal %-9s: %s' % (k, self.d_arch[k]) for k in sachdr.s_keys] )
-        s_lst.extend( ['externel %-9s: %s' % (k, self.d_arch[k]) for k in sub_keys] )
+        s_lst.extend( ['internal {:9}: {:f}'.format(k, self.d_arch[k]) for k in sachdr.f_keys if self.d_arch[k] != -12345.0 ] )
+        s_lst.extend( ['internal {:9}: {:d}'.format(k, self.d_arch[k]) for k in sachdr.i_keys  if self.d_arch[k] != -12345   ]  )
+        s_lst.extend( ['internal {:9}: {:}'.format(k, self.d_arch[k]) for k in sachdr.s_keys  if self.d_arch[k][:6] != '-12345' ] )
+        s_lst.extend( ['externel {:9}: {:}'.format(k, self.d_arch[k]) for k in sub_keys] )
         return '\n'.join(s_lst)
 class sactrace:
     """
@@ -336,75 +362,113 @@ class sactrace:
         """
         Write data into specified file.
         """
+        self.hdr.__update_npts__(np.size(self.dat) ) # update npts in case the dat length is revised.
         with open(filename, 'wb') as fid:
             fid.write(self.hdr.pack() )
             self.dat.astype(np.float32).tofile(fid)
     def read_2(self, filename, tmark, t1, t2):
         """
         Read sac data given filename and time window.
-        tmakr: 'b', 'e', 'o', 'a', 't0', 't1', ... 't9'.
+        tmark: 'b', 'e', 'o', 'a', 't0', 't1', ... 't9';
+        t1, t2: float;
         """
         self.read(filename)
+        self.truncate(tmark, t1, t2)
+    ### hdr methods
+    def update_hdr(self, **kwargs):
+        """
+        Update sac header.
+        kwargs: dict of (key: value), eg: {'delta': 0.1, 'kstnm': 'HYD}
+        """
+        self.hdr.update(**kwargs)
+    ### internel methods
+    def __get_t_idx__(self, tmark, t):
+        """
+        Given t, get the closest index.
+        """
         if tmark in self:
             t_ref = self[tmark]
             if t_ref == -12345.0:
                 print('unset header for tmark: %s %f', tmark, t_ref)
                 sys.exit(0)
-            i1 = max(int( np.round(t_ref - self['b'] + t1) / (self['delta'])  ), 0  )
-            i2 = min(int( np.round(t_ref - self['b'] + t2) / (self['delta'])  ), self.dat.size  ) 
-            # update data and header info
-            self.dat= self.dat[i1:i2]
-            self['npts'] = i2-i1
-            self['b'] = t_ref + t1
-            self['e'] = t_ref + t1 + (i2-i1) * self['delta']
+            idx = int(np.round( (t_ref+t-self['b'])/self['delta'] ) )
+            idx = min(max(idx, 0), self['npts']-1 )
+            return idx
         else:
             print('Unrecognized tmark for sactrace.read_sac_2(...) ', tmark)
             sys.exit(0)
-        pass
-    ### hdr methods
-    def update_hdr(self, **kwargs):
-        self.hdr.update(**kwargs)
-    ### internel methods
     def __getitem__(self, key):
         """
         Accessing with specified key.
         """
+        if key == 'dat':
+            return self.dat
         return self.hdr[key]
     def __setitem__(self, key, value):
         """
         Set value with specified key.
         """
+        if key == 'dat':
+            self.dat = value
+            self.hdr['npts'] = np.size(value)
         self.hdr[key] = value
     def __contains__(self, key):
         """
         Check whether a key in included.
         """
+        if key == 'dat' and self.dat:
+            return True
         return key in self.hdr
+    def __str__(self):
+        s = self.hdr.__str__()
+        s += '\ndata: (%d) ' % np.size(self.dat)
+        s += self.dat.__str__()
+        return s
+    def __update_dat__(self, dat):
+        """
+        Update self.dat, and revise npts, e in hdr.
+        """
+        self.dat = dat
+        self.hdr.__update_npts__(np.size(dat) )
     ### numerical methods
     def get_time_axis(self):
         """
         Get time axis.
         """
         return np.arange(0.0, self['npts'], 1.0) * self['delta'] + self['b']
+    def truncate(self, tmark, t1, t2):
+        """
+        Truncate given reference tmark, and time window.
+        tmark: 'b', 'e', 'o', 'a', 't0', 't1', ... 't9';
+        t1, t2: float;
+        """
+        i1 = self.__get_t_idx__(tmark, t1)
+        i2 = self.__get_t_idx__(tmark, t2) + 1
+        # update data and header info
+        self.dat= self.dat[i1:i2]
+        self['npts'] = i2-i1
+        self['b'] = self['b'] + i1*self['delta']
+        self['e'] = self['b'] + (self['npts']-1)*self['delta']
+        #return self
     def taper(self, ratio= 0.05):
         """
         Taper using tukey window.
         """
         w = signal.tukey(self['npts'], ratio)
         self.dat *= w
-        return self
+        #return self
     def rmean(self):
         """
         Remove mean value
         """
         self.dat -= np.average(self.dat)
-        return self
+        #return self
     def detrend(self):
         """
         Remove linear trend
         """
         self.dat = signal.detrend(self.dat)
-        return self
+        #return self
     def bandpass(self, f1, f2, order= 2, npass= 1):
         """
         Bandpass
@@ -415,7 +479,7 @@ class sactrace:
         b, a = signal.butter(order, [low, high], btype='bandpass')
         methods = [None, signal.lfilter, signal.filtfilt]
         self.dat = methods[npass](b, a, self.dat)
-        return self
+        #return self
     def lowpass(self, f, order= 2, npass= 1):
         """
         Lowpass
@@ -425,7 +489,7 @@ class sactrace:
         b, a = signal.butter(order, [fc], btype='lowpass')
         methods = [None, signal.lfilter, signal.filtfilt]
         self.dat = methods[npass](b, a, self.dat)
-        return self
+        #return self
     def highpass(self, f, order= 2, npass= 1):
         """
         High pass
@@ -435,7 +499,7 @@ class sactrace:
         b, a = signal.butter(order, [fc], btype='highpass')
         methods = [None, signal.lfilter, signal.filtfilt]
         self.dat = methods[npass](b, a, self.dat)
-        return self
+        #return self
     ### plot
     def plot_ax(self, ax, **kwargs):
         """
@@ -448,6 +512,7 @@ class sactrace:
         """
         plt.plot(self.get_time_axis(), self.dat, **kwargs)
         plt.show()
+        #plt.close()
     ###
     @classmethod
     def benchmark(cls):
@@ -464,7 +529,110 @@ class sactrace:
         ###
         #
         ###
+
+class sactrace_list(list):
+    """
+    Is this necessary ?
+    """
+    ### 
+    def __int__(self):
+        """
+        Empty constructor
+        """
+        pass
+    ### IO
+    def read_fnm_lst(self, fnm_lst):
+        """
+        Read a list of sac filenames for initialization.
+        """
+        self.extend( [rd_sac(fnm) for fnm in fnm_lst] )
+    def read_fnm_lst_2(self, fnm_lst, tmark, t1, t2):
+        """
+        Read a list of sac filenames with given time window setting for initialization.
+        tmark, t1, t2: time window setting for read.
+        """
+        self.extend( [rd_sac_2(fnm, tmark, t1, t2) for fnm in fnm_lst] )
+    def write(self, append_str):
+        """
+        Write, with filename appended by `append_str`.
+        append_str: string; (use '' for over write)
+        """
+        for it in self:
+            it.write(self['filename']+append_str)
+    ### inplace dat revision processing
+    def truncate(self, tmark, t1, t2):
+        """
+        """
+        for it in self:
+            it.truncate(tmark, t1, 2)
+    def taper(self, ratio= 0.05):
+        for it in self:
+            it.taper(ratio= ratio)
+    def bandpass(self, f1, f2, order=2, npass= 1):
+        for it in self:
+            it.bandpass(f1, f2, order= order, npass= npass)
+    def lowpass(self, f, order=2, npass= 1):
+        for it in self:
+            it.lowpass(f, order= order, npass= npass)
+    def highpass(self, f, order=2, npass= 1):
+        for it in self:
+            it.highpass(f, order= order, npass= npass)
+    ### sort
+    def sort(self, sachdr_key, sequence= 'ascend'):
+        """
+        Sort given sac header key.
+        sachdr_key: sac head key to used for sorting;
+        sequence: 'descend' or 'ascend';
+        """
+        #sorted(self, key=)
+        tmp = {'descend': -1, 'ascend': 1}
+        print(np.argsort([it[sachdr_key] for it in self ] )[::tmp[sequence] ])
+        lst = [self[idx] for idx in  np.argsort([it[sachdr_key] for it in self ] )[::tmp[sequence] ] ]
+        self.clear()
+        self.extend(lst)
+    ### plot
+    def plot1(self):
+        """
+        Similar to p1 in SAC. (need dev??)
+        """
+        ax = plt.subplot()
+        for idx, tr in enumerate(self):
+            ax.plot(tr.get_time_axis(), sactrace_list.normed(tr.dat)+idx )
+        plt.show()
+        #plt.close()
+    def plot2(self):
+        """
+        Similar to p2 in SAC.
+        """
+        ax = plt.subplot(111)
+        for  tr in self:
+            ax.plot(tr.get_time_axis(), tr.dat, label= tr['filename'] )
+            ax.legend(loc = 'upper right')
+        plt.show()
+    @staticmethod
+    def normed(trace):
+        scale = 1.0 / max(np.max(trace), -np.min(trace) )
+        return trace * scale
+
 if __name__ == "__main__":
+    wrt_sac_2('test.sac', np.array([1,2,3,4,5,6,7] ), 1.0, 0.0, **{'kstnm': 'aaaaaaaaaaaaaaaaaa', 'kevnm': 'bbbbbbbbbbbbbbbbbbbb'} )
+    s = rd_sac('test.sac')
+    print(s)
+    s['dat'] = np.array([1,2] )
+    print(s)
+    s.write('test2.sac')
+    s2 = rd_sac('test2.sac')
+    print(s)
+    #print([s['kt0'] ] )
+    #sys.exit(0)
+    vol  = sactrace_list()
+    vol.read_fnm_lst(['1.sac', '2.sac', '3.sac'])
+    vol.sort('mag', 'descend')
+    vol.plot2()
+    #sys.exit(0)
+    #for it in vol:
+    #    print( it['nzjday'])
+    #sys.exit(0)
     # io and basic processing
     s = rd_sac('1.sac')
     s.plot()       
@@ -484,7 +652,7 @@ if __name__ == "__main__":
     dat = np.random.random(1000)
     delta, b = 0.05, 50.0
     # writing method 1
-    write_sac_2('junk.sac', dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
+    wrt_sac_2('junk.sac', dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
     #
     # writing method 2, with processings
     s = make_sactrace_v(dat, delta, b, **{'kstnm': 'syn', 'stlo': 0.0, 'stla': 0.0} )
@@ -500,15 +668,21 @@ if __name__ == "__main__":
     new_hdr['t1'] = 2.0
     new_hdr['t2'] = 4.0
     new_hdr.update( **{'stlo': -10.0, 'stla': 10.0, 'delta': 0.2 } )
-    s_new = make_sactrace_hdr(s.dat, new_hdr)
+    s_new = make_sactrace_hdr(s['dat'], new_hdr)
     s_new.write('1_new.sac')
+    print(s_new)
     ###
     #
     ###
     # read, process, and write a bunch of sac files in 3 lines
     fnm_lst = ['1.sac', '2.sac', '3.sac']
-    vol = [rd_sac(fnm).detrend().taper().lowpass(1.2, order= 2, npass= 2) for fnm in fnm_lst]
-    [ s.write(s['filename'].replace('.sac', '_proced.sac') ) for s in vol ]
+    for fnm in fnm_lst:
+        s = rd_sac(fnm)
+        s.detrend()
+        s.taper()
+        s.lowpass(1.2, order= 2, npass= 2)
+        s.write(s['filename'].replace('.sac', '_proced.sac') )
+    #[ s.write(s['filename'].replace('.sac', '_proced.sac') ) for s in vol ]
     #import matplotlib.pyplot as plt
     #tr1 = np.array([0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0])
     #tr2 = np.array([0, 1, 2, 3, 2, 1, 0, 0, 0, 0, 0])
