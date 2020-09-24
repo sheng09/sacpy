20445e198b05142bf12cee8050d41ab8490af3d0
sheng09 <wangsheng.cas@gmail.com>
2020-09-24 12:20:38 +1000

    Fix bin/sf2d_bin2sac.py on compute2

--- bin/sf2d_bin2sac.py
+++ bin/sf2d_bin2sac.py
@@ -20,7 +20,7 @@ def plot_vr(figname, uv_mat, ur_mat, extent, t_range, d_range, title_v='Vertical
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(11, 10) )
     for ax, mat, title in zip([ax1, ax2], [uv_mat, ur_mat], [title_v, title_r]):
         vmax = mat.max() * 0.1
-        ax.imshow(mat, extent=extent, aspect='auto', cmap='seismic', vmax= vmax, vmin=-vmax)
+        ax.imshow(mat, extent=extent, aspect='auto', cmap='seismic', vmax= vmax, vmin=-vmax, origin='lower')
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Receiver longitude ($\degree$)')
         ax.set_xlim(t_range)
@@ -33,7 +33,7 @@ def plot_vr(figname, uv_mat, ur_mat, extent, t_range, d_range, title_v='Vertical
 def plot_xz(figname, ux_mat, uz_mat, extend, t_range, d_range, title_x='X', title_z='Z'):
     plot_vr(figname, ux_mat, uz_mat, extend, t_range, d_range, title_x, title_z)
 
-def run_out2sac(src_xz, rcv_fnm, delta, t0, ux_fnm, uz_fnm, data_type, prefilename, plot_t_range, plot_stlo_range):
+def run_out2sac(src_xz, rcv_fnm, delta, t0, ux_fnm, uz_fnm, data_type, prefilename, plot_t_range, plot_stlo_range, cmp):
     sx, sz = src_xz
     s_lon = np.arctan2(sz, sx)
     rcv_vol = np.loadtxt(rcv_fnm, dtype={'names': ['stnm', 'netwk', 'x', 'z', 'v4', 'v5' ], 
@@ -62,21 +62,34 @@ def run_out2sac(src_xz, rcv_fnm, delta, t0, ux_fnm, uz_fnm, data_type, prefilena
         sac_vertical = sac.make_sactrace_v(u_vertical, delta, t0, stlo=np.rad2deg(r_lon), stla=0.0, evlo=np.rad2deg(s_lon), evla=0.0)
         sac_radial   = sac.make_sactrace_v(u_radial,   delta, t0, stlo=np.rad2deg(r_lon), stla=0.0, evlo=np.rad2deg(s_lon), evla=0.0)
         ## output
-        for append, st in zip( ['X', 'Z', 'V', 'R'], [sac_x, sac_z, sac_vertical, sac_radial] ):
+        cmp_out = 'all'
+        sac_cmp_out = [sac_x, sac_z, sac_vertical, sac_radial]
+        if cmp == 'XZ':
+            cmp_out = ['X', 'Z']
+            sac_cmp_out = [sac_x, sac_z]
+        elif cmp == 'RV':
+            cmp_out = ['V', 'R']
+            sac_cmp_out = [sac_vertical, sac_radial]
+        else:
+            cmp_out = ['X', 'Z', 'V', 'R']
+            sac_cmp_out = [sac_x, sac_z, sac_vertical, sac_radial]
+        ###
+        for append, st in zip( cmp_out, sac_cmp_out ):
             fnm = '%s.%s.%s.%s.sac' % (prefilename, rcv['netwk'].decode('utf8'), rcv['stnm'].decode('utf8'), append)
             st.write(fnm)
     ####
     t1 = t0 + npts*delta
     lon0 = np.rad2deg(np.arctan2(rcv_vol[0]['z'], rcv_vol[0]['x']) ) % 360.0
     lon1 = np.rad2deg(np.arctan2(rcv_vol[-1]['z'], rcv_vol[-1]['x']) ) % 360.0
-    if plot_t_range != None and plot_stlo_range != None:
+    if plot_t_range != None and plot_stlo_range != None and (cmp == 'all' or cmp == 'XZ') :
         plot_xz(prefilename +'_xz.png', ux_mat, uz_mat, [t0, t1, lon0, lon1], plot_t_range, plot_stlo_range )
+    if plot_t_range != None and plot_stlo_range != None and (cmp == 'all' or cmp == 'RV') :
         plot_vr(prefilename +'_vr.png', uv_mat, ur_mat, [t0, t1, lon0, lon1], plot_t_range, plot_stlo_range )
 
 
 
 if __name__ == "__main__":
-    HMSG = '%s -S source_file -R receiver_file -D delta -B t_start -O pre_fnm [-P t0/t1/stlo0/stlo1] --ux ux.file --uz uz.file --accuracy=single'
+    HMSG = '%s -S source_file -R receiver_file -D delta -B t_start -O pre_fnm [-P t0/t1/stlo0/stlo1] --ux ux.file --uz uz.file --accuracy=single --cmp=[XZ|RV|all]'
     src_x, src_z = None, None
     rcv_fnm = None
     t0 = 0.0
@@ -86,7 +99,8 @@ if __name__ == "__main__":
     plot_t_range = None #[0.0, 3000.0]
     plot_stlo_range = None #[0.0, 180.0]
     data_type = np.float32
-    options, remainder = getopt.getopt(sys.argv[1:], 'S:R:D:B:O:P:H', ['ux=', 'uz=', 'accuracy='] )
+    cmp = 'all'
+    options, remainder = getopt.getopt(sys.argv[1:], 'S:R:D:B:O:P:H', ['ux=', 'uz=', 'accuracy=', 'cmp='] )
     for opt, arg in options:
         if opt in ('-S'):
             with open(arg, 'r') as fid:
@@ -110,6 +124,13 @@ if __name__ == "__main__":
                 data_type = np.float32
             elif arg == 'double':
                 data_type = np.float64
+        elif opt in ('--cmp'):
+            if arg in ('RV', 'VR'):
+                cmp = 'RV'
+            elif arg in ('XZ', 'ZX'):
+                cmp = 'XZ'
+            else:
+                cmp = 'all'
         elif opt in ('-O'):
             output_sac_prefnm = arg
         elif opt in ('-P'):
@@ -123,4 +144,4 @@ if __name__ == "__main__":
             print('invalid options: %s' % (opt) )
             print(HMSG % (sys.argv[0]) )
             sys.exit(0)
-    run_out2sac((src_x, src_z), rcv_fnm, delta, t0, ux_fnm, uz_fnm, data_type, output_sac_prefnm, plot_t_range, plot_stlo_range )
+    run_out2sac((src_x, src_z), rcv_fnm, delta, t0, ux_fnm, uz_fnm, data_type, output_sac_prefnm, plot_t_range, plot_stlo_range, cmp)
