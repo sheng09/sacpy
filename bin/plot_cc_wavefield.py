6752dffb6b166a96aedb016eec90f34fc33800d6
sheng09 <wangsheng.cas@gmail.com>
2020-12-04 12:12:39 +1100

    Add more pyplot options for bin/plot_cc_wavefield.py

--- bin/plot_cc_wavefield.py
+++ bin/plot_cc_wavefield.py
@@ -8,7 +8,10 @@ from getopt import getopt
 from sys import exit, argv
 import numpy as np
 
-def run(h5_filename, figname, dist_range=None, cc_time_range=None, lines= None, figsize= (6, 15), interpolation= None, title='' ):
+def run(h5_filename, figname, dist_range=None, cc_time_range=None, lines= None, 
+    figsize= (6, 15), interpolation= None, title='', vmax= 1.0, axhist=True, ylabel=True, grid=False ):
+    """
+    """
     fid = h5_File(h5_filename, 'r')
     cc_t0, cc_t1 = fid['ccstack'].attrs['cc_t0'], fid['ccstack'].attrs['cc_t1']
     mat = fid['ccstack'][:]
@@ -21,16 +24,20 @@ def run(h5_filename, figname, dist_range=None, cc_time_range=None, lines= None, 
         i2 = int( np.round((cc_time_range[1]-cc_t0)/delta) )
         mat = mat[:, i1:i2]
         cc_t0, cc_t1 = cc_time_range
-    mat = mat.transpose()
     for irow in range(dist.size):
-        v = mat[irow].max()
+        v = max(mat[irow].max(), -mat[irow].min() )
         if v > 0.0:
             mat[irow] *= (1.0/v)
+    mat = mat.transpose()
     ###
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize= figsize, gridspec_kw={'height_ratios': [5, 1]} )
+    ax1, ax2 = None, None
+    if axhist:
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize= figsize, gridspec_kw={'height_ratios': [5, 1]} )
+    else:
+        fig, ax1 = plt.subplots(1, 1, figsize= figsize )
+    ###
     ax1.imshow(mat, extent=(dist[0], dist[-1], cc_t0, cc_t1 ), aspect='auto', cmap='gray', interpolation= interpolation,
-            vmin=-0.6, vmax=0.6, origin='lower' )
-    ax2.bar(dist, stack_count, align='center', color='gray', width= dist[1]-dist[0] )
+            vmin=-vmax, vmax=vmax, origin='lower')
     ###
     if lines != None:
         for d, t in lines:
@@ -39,16 +46,26 @@ def run(h5_filename, figname, dist_range=None, cc_time_range=None, lines= None, 
     dist_range = (dist[0], dist[-1] ) if dist_range == None else dist_range
     ax1.set_xlim(dist_range)
     ax1.set_xlabel('Inter-receiver distance ($\degree$)')
-    ax1.set_ylabel('Correlation time (s)')
+    if ylabel:
+        ax1.set_ylabel('Correlation time (s)')
+    else:
+        ax1.set_yticklabels([])
+    if grid:
+        ax1.grid(linestyle=':', color='k' )
     if cc_time_range:
         ax1.set_ylim(cc_time_range)
     ax1.set_title(title)
     ###
-    tmp = stack_count[stack_count>0]
-    ax2.set_xlim(dist_range)
-    ax2.set_ylim((0, sorted(tmp)[-2] * 1.1) )
-    ax2.set_ylabel('Number of receiver pairs')
-    ax2.set_xlabel('Inter-receiver distance ($\degree$)')
+    if axhist:
+        tmp = stack_count[stack_count>0]
+        ax2.bar(dist, stack_count, align='center', color='gray', width= dist[1]-dist[0] )
+        ax2.set_xlim(dist_range)
+        ax2.set_ylim((0, sorted(tmp)[-2] * 1.1) )
+        ax2.set_xlabel('Inter-receiver distance ($\degree$)')
+        if ylabel:
+            ax2.set_ylabel('Number of receiver pairs')
+        else:
+            ax2.set_yticklabels([])
     plt.savefig(figname, bbox_inches = 'tight', pad_inches = 0.2)
     plt.close()
 
@@ -58,6 +75,10 @@ def plt_options(args):
     figsize = (6, 15)
     interpolation = None
     title = ''
+    vmax = 1.0
+    axhist = True
+    ylabel = True
+    grid   = False
     ###
     for it in args.split(','):
         opt, value = it.split('=')
@@ -67,7 +88,15 @@ def plt_options(args):
             interpolation = value
         elif opt == 'title':
             title = value
-    return figsize, interpolation, title
+        elif opt == 'vmax':
+            vmax = float(value)
+        elif opt == 'axhist':
+            axhist = True if value == 'True' else False
+        elif opt == 'ylabel':
+            ylabel = True if value == 'True' else False
+        elif opt == 'grid':
+            grid = True if value == 'True' else False
+    return figsize, interpolation, title, vmax, axhist, ylabel, grid
 
 def get_lines(fnms):
     """
@@ -89,11 +118,15 @@ if __name__ == "__main__":
     figsize = (6, 15)
     interpolation = None
     title = ''
+    vmax = 1.0
+    axhist = True
+    ylabel = True
+    grid = False
     #### lines to plot
     lines = None
     ####
     HMSG = """
-    %s -I in.h5 -P img.png [-D 0/50] [-T 0/3000] [--lines fnm1,fnm2,fnm3] [--plt figsize=6/12,interpolation=gaussian] [-H]
+    %s -I in.h5 -P img.png [-D 0/50] [-T 0/3000] [--lines fnm1,fnm2,fnm3] [--plt figsize=6/12,interpolation=gaussian,title=all,vmax=1.0,axhist=False,ylabel=True,grid=False] [-H]
     """ % argv[0]
     if len(argv) < 2:
         print(HMSG)
@@ -112,10 +145,11 @@ if __name__ == "__main__":
         elif opt in ('--lines'):
             lines = get_lines(arg)
         elif opt in ('--plt'):
-            figsize, interpolation, title = plt_options(arg)
+            figsize, interpolation, title, vmax, axhist, ylabel, grid = plt_options(arg)
         else:
             print(HMSG)
             exit(0)
     ####
-    run(h5_fnm, figname, dist_range, cc_time_range, lines, figsize, interpolation, title)
+    run(h5_fnm, figname, dist_range, cc_time_range, lines, 
+            figsize, interpolation, title, vmax, axhist, ylabel, grid)
 
