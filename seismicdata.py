a63b09a5f6402c38f6ed92d3edd08576e40e1f07
sheng09 <wangsheng.cas@gmail.com>
2023-01-24 19:51:44 +1100

    Add plot functions into Waveforms, and an independent spectrogram_ax function

--- seismicdata.py
+++ seismicdata.py
@@ -10,10 +10,14 @@ from obspy.core.inventory.inventory import Inventory
 import pickle
 from sacpy.utils import send_email, get_http_files, wget_http_files
 from h5py import File as h5_File
-from numpy import float32, int32, zeros
+from numpy import float32, int32, zeros, array
 from numpy import max as np_max
 from numpy import min as np_min
 from sacpy.geomath import haversine, azimuth
+import math
+from matplotlib import mlab
+from matplotlib.colors import Normalize
+import numpy as np
 
 class BreqFast:
     """
@@ -556,6 +560,118 @@ class Waveforms(Stream):
             sac_dict['mag'] = mag
             #
             sac_dict['o'] = 0.0
+    def plot_wv(self, ax, reftime=None, baselines=None, normalize=False, scale=None, color='k', fill_between=None, **kwargs):
+        """
+        Plot the waveforms into an existed `ax`. This function will not change the data.
+
+        reftime: an single `UTCDateTime` object or a list of `UTCDateTime` object.
+                 The `reftime` will be used to compute the relative time of time axies.
+                 In default, set `reftime=None` to use the `starttime` for each trace.
+        baselines: a list of floating values for the baselines to plot each of the traces.
+                   In default, set `baselines=None` to automatically adjust the waveforms to avoid overlapping.
+        normalize: `False`(default) or a float value to normalize in respect with the maximal positive amplitude.
+        scale: a single or a list of floating values to scale the waveforms.
+               In default, set `scale=None` to disable this option.
+        fill_between: 'positive' or 'negative' to fill the postive or negative amplitudes, respectively.
+               In default, set `scale=None` to disable this option.
+        """
+        ##############
+        st = self
+        if normalize:
+            st = self.copy()
+            st.detrend()
+            for tr in st:
+                tr.data *= (normalize/tr.data.max() )
+        if type(scale)!=type(None):
+            try:
+                n = len(scale)
+                if n!=len(st):
+                    errmsg = "Err: number of scale does not match the number of traces. %d!=%d " % (n, len(st))
+                    raise RuntimeError(errmsg)
+                else:
+                    for tr, s in zip(st, scale):
+                        tr.data *= s
+            except Exception:
+                for tr in st:
+                    tr.data *= scale
+        ##############
+        if type(reftime)!=type(None):
+            try:
+                n = len(reftime)
+                if n!=len(st):
+                    errmsg = "Err: number of reftime does not match the number of traces. %d!=%d " % (n, len(st))
+                    raise RuntimeError(errmsg)
+            except Exception:
+                reftime = [reftime for it in st]
+        else:
+            reftime = [it.stats.starttime for it in st]
+        ###############
+        if type(baselines)!=type(None):
+            try:
+                n = len(baselines)
+                if n!=len(st):
+                    errmsg = "Err: number of baselines does not match the number of traces. %d!=%d " % (n, len(st))
+                    raise RuntimeError(errmsg)
+            except Exception:
+                errmsg = "Err: set a list of numbers for baselines"
+                raise RuntimeError(errmsg)
+        else:
+            baselines = list()
+            _base = 0.0
+            for tr in st:
+                _base = _base - tr.data.min()
+                baselines.append(_base)
+                _base = _base + tr.data.max()
+        baselines = array(baselines)
+        ###############
+        for tr, tref, _base in zip(st, reftime, baselines):
+            ys = tr.data + _base
+            ts = tr.times(type='relative', reftime=tref)
+            ax.plot(ts, ys, color=color, **kwargs)
+            if fill_between == 'positive':
+                ax.fill_between(ts, ys, _base, where=ys>=_base, interpolate=True, color=color, alpha=0.3)
+            elif fill_between == 'negative':
+                ax.fill_between(ts, ys, _base, where=ys<=_base, interpolate=True, color=color, alpha=0.3)
+
+        return baselines
+    def plot_wavefield(self, ax, normalize=False, scale=None, cmap='gray', interpolation=None, vmin=None, vmax=None, extent=None):
+        """
+        Plot the wavefield into an existed `ax`. This function will not change the data.
+
+        normalize: `False`(default) or a float value to normalize in respect with the maximal positive amplitude.
+        scale: a single or a list of floating values to scale the waveforms.
+               In default, set `scale=None` to disable this option.
+        """
+        ##############
+        st = self
+        if normalize:
+            st = self.copy()
+            st.detrend()
+            for tr in st:
+                tr.data *= (normalize/tr.data.max() )
+        if type(scale)!=type(None):
+            try:
+                n = len(scale)
+                if n!=len(st):
+                    errmsg = "Err: number of scale does not match the number of traces. %d!=%d " % (n, len(st))
+                    raise RuntimeError(errmsg)
+                else:
+                    for tr, s in zip(st, scale):
+                        tr.data *= s
+            except Exception:
+                for tr in st:
+                    tr.data *= scale
+        ##############
+        ncol = np_max([tr.data.size for tr in st])
+        nrow = len(st)
+        mat = zeros((nrow, ncol), dtype=float32)
+        for idx, tr in enumerate(st):
+            mat[idx,:][:tr.data.size] = tr.data
+        ##############
+        if type(extent) == type(None):
+            extent = array((0.0, st[0].stats.delta*(ncol-1), 0.0, nrow) )
+        ax.imshow(mat, origin="lower", cmap=cmap, interpolation=interpolation, vmin=vmin, vmax=vmax, aspect="auto", extent=extent)
+        return extent
     def to_hdf5(self, h5_filename, info=''):
         """
         Convert the time-series and their metadata to hdf5 format file.
@@ -754,6 +870,173 @@ class Converter:
             fid.create_dataset('dat', data=mat, dtype=float32)
         #####################################################################################################################
         fid.close()
+
+def _nearest_pow_2(x):
+    """
+    Find power of two nearest to x
+
+    >>> _nearest_pow_2(3)
+    2.0
+    >>> _nearest_pow_2(15)
+    16.0
+
+    :type x: float
+    :param x: Number
+    :rtype: int
+    :return: Nearest power of 2 to x
+    """
+    a = math.pow(2, math.ceil(np.log2(x)))
+    b = math.pow(2, math.floor(np.log2(x)))
+    if abs(a - x) < abs(b - x):
+        return a
+    else:
+        return b
+def spectrogram_ax(axes, data, samp_rate, per_lap=0.9, wlen=None, log=False,
+                outfile=None, fmt=None, dbscale=False,
+                mult=8.0, cmap='plasma', zorder=None, title=None,
+                show=True, clip=[0.0, 1.0]):
+    """
+    Computes and plots spectrogram of the input data.
+
+    :param data: Input data
+    :type samp_rate: float
+    :param samp_rate: Samplerate in Hz
+    :type per_lap: float
+    :param per_lap: Percentage of overlap of sliding window, ranging from 0
+        to 1. High overlaps take a long time to compute.
+    :type wlen: int or float
+    :param wlen: Window length for fft in seconds. If this parameter is too
+        small, the calculation will take forever. If None, it defaults to a
+        window length matching 128 samples.
+    :type log: bool
+    :param log: Logarithmic frequency axis if True, linear frequency axis
+        otherwise.
+    :type outfile: str
+    :param outfile: String for the filename of output file, if None
+        interactive plotting is activated.
+    :type fmt: str
+    :param fmt: Format of image to save
+    :type axes: :class:`matplotlib.axes.Axes`
+    :param axes: Plot into given axes, this deactivates the fmt and
+        outfile option.
+    :type dbscale: bool
+    :param dbscale: If True 10 * log10 of color values is taken, if False the
+        sqrt is taken.
+    :type mult: float
+    :param mult: Pad zeros to length mult * wlen. This will make the
+        spectrogram smoother.
+    :type cmap: :class:`matplotlib.colors.Colormap`
+    :param cmap: Specify a custom colormap instance. If not specified, then the
+        default ObsPy sequential colormap is used.
+    :type zorder: float
+    :param zorder: Specify the zorder of the plot. Only of importance if other
+        plots in the same axes are executed.
+    :type title: str
+    :param title: Set the plot title
+    :type show: bool
+    :param show: Do not call `plt.show()` at end of routine. That way, further
+        modifications can be done to the figure before showing it.
+    :type clip: [float, float]
+    :param clip: adjust colormap to clip at lower and/or upper end. The given
+        percentages of the amplitude range (linear or logarithmic depending
+        on option `dbscale`) are clipped.
+    """
+    import matplotlib.pyplot as plt
+    # enforce float for samp_rate
+    samp_rate = float(samp_rate)
+
+    # set wlen from samp_rate if not specified otherwise
+    if not wlen:
+        wlen = 128 / samp_rate
+
+    npts = len(data)
+
+    # nfft needs to be an integer, otherwise a deprecation will be raised
+    # XXX add condition for too many windows => calculation takes for ever
+    nfft = int(_nearest_pow_2(wlen * samp_rate))
+
+    if npts < nfft:
+        msg = (f'Input signal too short ({npts} samples, window length '
+               f'{wlen} seconds, nfft {nfft} samples, sampling rate '
+               f'{samp_rate} Hz)')
+        raise ValueError(msg)
+
+    if mult is not None:
+        mult = int(_nearest_pow_2(mult))
+        mult = mult * nfft
+    nlap = int(nfft * float(per_lap))
+
+    data = data - data.mean()
+    end = npts / samp_rate
+
+    # Here we call not plt.specgram as this already produces a plot
+    # matplotlib.mlab.specgram should be faster as it computes only the
+    # arrays
+    # XXX mlab.specgram uses fft, would be better and faster use rfft
+    specgram, freq, time = mlab.specgram(data, Fs=samp_rate, NFFT=nfft,
+                                         pad_to=mult, noverlap=nlap)
+
+    if len(time) < 2:
+        msg = (f'Input signal too short ({npts} samples, window length '
+               f'{wlen} seconds, nfft {nfft} samples, {nlap} samples window '
+               f'overlap, sampling rate {samp_rate} Hz)')
+        raise ValueError(msg)
+
+    # db scale and remove zero/offset for amplitude
+    if dbscale:
+        specgram = 10 * np.log10(specgram[1:, :])
+    else:
+        specgram = np.sqrt(specgram[1:, :])
+    freq = freq[1:]
+
+    vmin, vmax = clip
+    if vmin < 0 or vmax > 1 or vmin >= vmax:
+        msg = "Invalid parameters for clip option."
+        raise ValueError(msg)
+    _range = float(specgram.max() - specgram.min())
+    vmin = specgram.min() + vmin * _range
+    vmax = specgram.min() + vmax * _range
+    norm = Normalize(vmin, vmax, clip=True)
+
+    ax = axes
+
+    # calculate half bin width
+    halfbin_time = (time[1] - time[0]) / 2.0
+    halfbin_freq = (freq[1] - freq[0]) / 2.0
+
+    # argument None is not allowed for kwargs on matplotlib python 3.3
+    kwargs = {k: v for k, v in (('cmap', cmap), ('zorder', zorder))
+              if v is not None}
+
+    if log:
+        # pcolor expects one bin more at the right end
+        freq = np.concatenate((freq, [freq[-1] + 2 * halfbin_freq]))
+        time = np.concatenate((time, [time[-1] + 2 * halfbin_time]))
+        # center bin
+        time -= halfbin_time
+        freq -= halfbin_freq
+        # Log scaling for frequency values (y-axis)
+        ax.set_yscale('log')
+        # Plot times
+        im = ax.pcolormesh(time, freq, specgram, norm=norm, **kwargs)
+    else:
+        # this method is much much faster!
+        specgram = np.flipud(specgram)
+        # center bin
+        extent = (time[0] - halfbin_time, time[-1] + halfbin_time,
+                  freq[0] - halfbin_freq, freq[-1] + halfbin_freq)
+        im = ax.imshow(specgram, interpolation="nearest", extent=extent, **kwargs)
+
+    # set correct way of axis, whitespace before and after with window
+    # length
+    ax.axis('tight')
+    ax.set_xlim(0, end)
+    ax.grid(False)
+    ####    
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Frequency [Hz]')
+
+    return im
 
 if __name__ == '__main__':
     if True:
